22b25492f3434206893c1890de16670e373592e2
Andrei <anton.andreii@example.com>
2020-08-09 19:18:13 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
+import Popup from './components/Popup';
+import Notification from './components/Notification';
+import { showNotification as show} from './helpers/helpers';
+
+import './App.css';
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+function App() {
+  const [ playable, setPlayable ] = useState(true);
+  const [ correctLetters, setCorrectLetters ] = useState([]);
+  const [ wrongLetters, setWrongLetters ] = useState([]);
+  const [ showNotification, setShowNotification ] = useState(false);
+
+  useEffect(() => {
+    const handleKeyDown = event => {
+      const { key, keyCode} = event;
+      // 65 - 90 are letter codes on the keyboard
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters(currectLetters => [...currectLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters(wrongLetters => [...wrongLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        }
+      }
+    }
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => window.removeEventListener('keydown', handleKeyDown)
+  }, [correctLetters, wrongLetters, playable]);
+
+  function playAgain() {
+      setPlayable(true);
+
+      // empty arrays
+      setCorrectLetters([]);
+      setWrongLetters([]);
+
+      const random = Math.floor(Math.random() * words.length);
+      selectedWord = words[random];
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
+      <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} selectedWord=
+      {selectedWord} setPlayable={setPlayable} playAgain={playAgain} />
+      <Notification showNotification={showNotification} />
+    </>
+  );
+}
+
+export default App;
